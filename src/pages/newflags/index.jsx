bed07bf6825054e90ce4988d809fd69a2b425969
Jerry.jiao <jerry201382@gmail.com>
2020-06-06 18:20:01 +0800

    feat: add react-hook-form

--- src/pages/newflags/index.jsx
+++ src/pages/newflags/index.jsx
@@ -3,20 +3,46 @@ import { connect } from 'dva'
 import './index.less'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+import { useForm, Controller } from "react-hook-form"
 
 const NewFlags = (props)=>{
+  const { handleSubmit, control } = useForm()
   const onSubmit = (data, e) => {
     console.log('data----->',data)
     console.log('e------>',e)
   }
   return(
     <div>
-      <form action="" className="newflags-container" noValidate autoComplete="off" onSubmit={onSubmit}>
+      <form action="" className="newflags-container" noValidate autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
         <div className="newflags-title">新建立志</div>
-        <TextField id="task-basic" label="任务" variant="filled" className="newflags-item"></TextField>
-        <TextField id="days-basic" label="天数" variant="filled" className="newflags-item"></TextField>
-        <TextField id="maxWitness-basic" label="最大奖励" variant="filled" className="newflags-item"></TextField>
-        <TextField id="maxWitness-basic" label="完成时间" variant="filled" className="newflags-item"></TextField>
+        <Controller 
+          as={<TextField id="task-basic" label="任务" variant="filled" className="newflags-item"></TextField>}
+          control={control}
+          defaultValue=''
+        />
+
+       <Controller 
+          as={
+            <TextField id="days-basic" label="天数" variant="filled" className="newflags-item"></TextField>
+          }
+          control={control}
+          defaultValue=''
+       />
+
+       <Controller 
+          as={
+            <TextField id="maxWitness-basic" label="最大奖励" variant="filled" className="newflags-item"></TextField>
+          }
+          control={control}
+          defaultValue=''
+       />
+        <Controller 
+          as={
+            <TextField id="maxWitness-basic" label="完成时间" variant="filled" className="newflags-item"></TextField>
+          }
+          control={control}
+        />
+
         <Button variant="contained" color="primary" className="newflags-submit" size="large" type="submit">
            点击新建
         </Button>
