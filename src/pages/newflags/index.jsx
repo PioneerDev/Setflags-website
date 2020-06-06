3456ce00aa6af41a5531d6d97f160d47d2d19c04
Jerry.jiao <jerry201382@gmail.com>
2020-06-06 17:33:58 +0800

    feat(newflag): ui finish

--- src/pages/newflags/index.jsx
+++ src/pages/newflags/index.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import { connect } from 'dva'
 import './index.less'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 const NewFlags = (props)=>{
+  const onSubmit = (data, e) => {
+    console.log('data----->',data)
+    console.log('e------>',e)
+  }
   return(
     <div>
-      111
+      <form action="" className="newflags-container" noValidate autoComplete="off" onSubmit={onSubmit}>
+        <div className="newflags-title">新建立志</div>
+        <TextField id="task-basic" label="任务" variant="filled" className="newflags-item"></TextField>
+        <TextField id="days-basic" label="天数" variant="filled" className="newflags-item"></TextField>
+        <TextField id="maxWitness-basic" label="最大奖励" variant="filled" className="newflags-item"></TextField>
+        <TextField id="maxWitness-basic" label="完成时间" variant="filled" className="newflags-item"></TextField>
+        <Button variant="contained" color="primary" className="newflags-submit" size="large" type="submit">
+           点击新建
+        </Button>
+      </form>
     </div>
   )
 }
