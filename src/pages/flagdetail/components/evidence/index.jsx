a30b6f4d98c9ed63c52a0c9bf65a3e428b2171a2
Jerry.jiao <jerry201382@gmail.com>
2020-06-19 23:00:01 +0800

    feat: flagdetial样式

--- src/pages/flagdetail/components/evidence/index.jsx
+++ src/pages/flagdetail/components/evidence/index.jsx
@@ -4,21 +4,23 @@ import './index.less'
 
 const Evidence = (props)=>{
   return (
-    <div>
-      <EvidenceItem></EvidenceItem>
+    <div className="evidence-container">
+      <EvidenceItem evidenceUrl="http://dummyimage.com/100x150" time="2020-06-21 22:00:00"></EvidenceItem>
+      <EvidenceItem evidenceUrl="http://dummyimage.com/100x150" time="2020-06-21 22:00:00"></EvidenceItem>
     </div>
   )
 }
 
 
 const EvidenceItem = (props)=>{
-  const { evidenceUrl, time } = this.props
+  const { evidenceUrl, time } = props
+  console.log('ev--->',evidenceUrl)
   return(
-    <div>
-      <div>
-        <image src={evidenceUrl}></image>
+    <div className="evidence-item">
+      <div className="evidence-item-media">
+        <img src={evidenceUrl} alt="evidenceitm"></img>
       </div>
-      <div>{time}</div>
+      <div className="evidence-item-time">{time}</div>
     </div>
   )
 }
--- src/pages/flagdetail/components/rewards/index.jsx
+++ src/pages/flagdetail/components/rewards/index.jsx
@@ -4,8 +4,27 @@ import './index.less'
 
 const Rewards = (props)=>{
   return (
-    <div>
-      rewards
+    <div className="rewards-container">
+      <RewardsItem avatarurl="https://mixin-images.zeromesh.net/gj-3TbH_lYSvwLtp-gG_B8w6X68Jg6JqIojf5BLAA54xW0AxQXsOefMlFVbDgYbILs4hsBhqAeD4THfzqce-Enqq=s256" name="凯" time="2020-06-20 10:09:00" amount={1000} unit="Box"/>
+    </div>
+  )
+}
+
+const RewardsItem = (props) => {
+  const {avatarurl, name, time, amount, unit} = props
+  return (
+    <div className="rewards-item">
+      <div className="rewards-item-info">
+        <div className="rewards-item-avatar">
+          <img src={avatarurl} alt="rewardsavatar"/>
+        </div>
+        {/* 一行隐藏 */}
+        <div className="rewards-item-txt">
+          <div className="rewards-item-name">{name}</div>
+          <div className="rewards-item-time">{time}</div>
+        </div>
+      </div>
+  <div className="rewards-item-amount">{amount} <span className="rewards-item-unit">{unit}</span></div>
     </div>
   )
 }
