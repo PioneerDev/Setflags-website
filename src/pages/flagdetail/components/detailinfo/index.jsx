175d3908a40b01ed628ab874838b4ea69668dcdc
Jerry.jiao <jerry201382@gmail.com>
2020-06-20 22:48:02 +0800

    feat: flagdetail ui finish

--- src/pages/flagdetail/components/detailinfo/index.jsx
+++ src/pages/flagdetail/components/detailinfo/index.jsx
@@ -6,13 +6,14 @@ const DetailInfo = (props)=>{
   return (
     <div className="detailinfo-container">
       <div className="detailinfo-info">
-        <div className="detailinfo-avatar">
-          <img src="https://mixin-images.zeromesh.net/ml7tg1ZGrQt6IJSvEusWFfthosOB98GWN7r4EkmgSP8tbJHxK7OWki9zfZFFDCDOJE0nlLBR6dc4nbUguXE3Bg4=s128" alt=""/>
-          <div>Jerry</div>
-          <div>做10次俯卧撑</div>
+         <div className="detailinfo-avatar">
+            <img src="https://mixin-images.zeromesh.net/ml7tg1ZGrQt6IJSvEusWFfthosOB98GWN7r4EkmgSP8tbJHxK7OWki9zfZFFDCDOJE0nlLBR6dc4nbUguXE3Bg4=s128" alt=""/>
+          </div>
+          <div className="detailinfo-name">Jerry</div>
+          <div className="detailinfo-task">做10次俯卧撑</div>
         </div>
         <div className="detailinfo-button">
-          <Button variant="contained" color="primary">
+          <Button variant="contained" color="primary" style={{marginRight:10}}>
             已完成
           </Button>
           <Button variant="contained" color="secondary">
@@ -20,8 +21,7 @@ const DetailInfo = (props)=>{
           </Button>
         </div>
         {/* //TODO: comment*/}
-      </div>
-      <div>1BOX</div>
+      <div className="detailinfo-rewards">1.000000000<span className="detailinfo-unit">BOX</span></div>
     </div>
   )
 }
