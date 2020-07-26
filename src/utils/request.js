fead01ac00f5bda797acfc390c52f507a8352dc6
Jerry.jiao <jerry201382@gmail.com>
2020-07-26 20:58:17 +0800

    feat: err message

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,12 @@
 
 import { extend } from 'umi-request';
+import Toastify from 'toastify-js'
 
 const {BASEURL:baseurl} = process.env
 // 402就是登录失效
 const errorHandler = (error)=> {
   console.log("errorHandler -> error", error)
-  const { response } = error;
+  const { response, data } = error;
   if (response && response.status) {
     // const { status, url } = response;
     if(response.status === 401) {
@@ -21,6 +22,23 @@ const errorHandler = (error)=> {
   } else if (!response) {
     console.error('network error');
   }
+
+  if(data && data.code) {
+    if(data.code !== 200) {
+      Toastify({
+        text: `${data.msg}`,
+        duration: 30000, 
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "bottom", // `top` or `bottom`
+        position: 'left', // `left`, `center` or `right`
+        backgroundColor: "red",
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        onClick: function(){} // Callback after click
+      }).showToast();
+    }
+  } 
   return response;
 };
 
